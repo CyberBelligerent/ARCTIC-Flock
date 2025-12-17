37f76bed27829cc217f4d27022bdeb7f3dc2c3da
Sean Rahman <35696076+SpaceXElaborator@users.noreply.github.com>
2025-12-17 21:26:09 +0100

    Fixed Scanning Issues
    Moved scanning to entire base package of com.rahman.arctic instead of pulling the specific objects and repos...

--- src/main/java/com/rahman/arctic/flock/SpringEntry.java
+++ src/main/java/com/rahman/arctic/flock/SpringEntry.java
@@ -2,8 +2,14 @@ package com.rahman.arctic.flock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.rahman.arctic.flock", "com.rahman.arctic.orca", "com.rahman.arctic.iceberg", "com.rahman.arctic.shard"})
+@SpringBootApplication(scanBasePackages = {"com.rahman.arctic"})
+//@EnableJpaRepositories(basePackages = {"com.rahman.arctic.iceberg.repos", "com.rahman.arctic.polarbear.repos", "com.rahman.arctic.shard.repos"})
+//@EntityScan(basePackages = {"com.rahman.arctic.iceberg.objects", "com.rahman.arctic.polarbear.objects", "com.rahman.arctic.shard.objects"})
+@EnableJpaRepositories(basePackages = {"com.rahman.arctic"})
+@EntityScan(basePackages = {"com.rahman.arctic"})
 public class SpringEntry {
 	
 	public static void main(String[] args) {
